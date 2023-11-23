52425280562fc18736197c9fba517612e2a6a63b
PixBlezz <148706507+PixBlezz@users.noreply.github.com>
2023-11-23 16:39:56 +0000

    my phd courses page

---
+++ phd.js
@@ -0,0 +1,77 @@
+function phd(){
+    document.querySelector('.js_1').innerHTML=`FIRST SEMESTER`
+}
+function course1(){
+    document.querySelector('.course1').innerHTML=`FASC 701	Science and Society (Faculty Wide)`
+}
+function course2(){
+    document.querySelector('.course2').innerHTML=`FASC 700	Special Topics in Science`
+}
+
+
+
+
+function phd1(){
+    document.querySelector('.js_2').innerHTML=`SECOND SEMESTER`
+}
+function course3(){
+    document.querySelector('.course3').innerHTML=`CSCD 721	Grand Research Challenges in Computer Science
+    
+    `
+}
+function course4(){
+    document.querySelector('.course4').innerHTML=`CSCD 723	Emerging Database Models
+   `
+}
+
+
+function phd2(){
+    document.querySelector('.js_3').innerHTML=`FIRST SEMESTER`
+}
+function course5(){
+    document.querySelector('.course5').innerHTML=` CSCD 725	Research in Bioinformatics and Computational Biology
+   `
+}
+function course6(){
+    document.querySelector('.course6').innerHTML=` CSCD 727	Ubiquitous Computing
+    
+   `
+}
+
+
+
+function phd3(){
+    document.querySelector('.js_3').innerHTML=`SECOND SEMESTER`
+}
+function course7(){
+    document.querySelector('.course7').innerHTML=`  
+    FASC 710	Teaching Science at the Tertiary Level)
+    `
+}
+function course8(){
+    document.querySelector('.course8').innerHTML=`CSCD 722	New Developments in Cyber Security and Networks
+   `
+}
+
+function phd4(){
+    document.querySelector('.js_4').innerHTML=`FIRST SEMESTER`
+}
+function course9(){
+    document.querySelector('.course9').innerHTML=` FASC 702	Advanced Quantitative Research Methods
+   `
+}
+function course10(){
+    document.querySelector('.course10').innerHTML=`
+    CSCD 724	New Developments in Computer Architecture
+    `
+}
+function organ(){
+    document.querySelector('.org_word').innerHTML=`The University of Ghana is one of the Africa's most preeminent public universities. Our impact on individuals, our region and the world is profound. whether we are launching young people into a boundless future or confronting the grand challenges of our time through dauntless research. UG educates more than 38,000students annually, with approximately 2.64% being computer science students. We turn ideas into impact and transform the world around us. For more about our impact, visit our news site, UG News.`;
+}
+function organ1(){
+    document.querySelector('.org_word1').innerHTML=`If you are inspired by a big challenge, you've come to the right place! The computer science community at the univrersity of Ghana encourages it's people to be vibrant and brave in taking on new challenges that push boundries beyond imagination.`;
+}function organ2(){
+    document.querySelector('.org_word2').innerHTML=`Experience a UG-CDCD classroom from wherever you are. Take advantage of real-time lectures and personal interaction with professors through a suite of online delivery options, customizing your degree to fit your schedule. Our online classroom maintains the same level of academic rigor, while providing students with more ways of learning.`;
+}function organ3(){
+    document.querySelector('.org_word3').innerHTML=`So what defines our students, faculty and community members? Above all, its our belief in the infinite possibilitie and our unshakable optimism. Its a connection to others near and far. A hunger for socio-economic transformation that pushes us to tackle challenges and pursue progress. Its the conviction that together we can create a world of good. Join us on the journey.`;
+}
