b4378bc65f82584d84d27b888132f29f054040e3
PixBlezz <148706507+PixBlezz@users.noreply.github.com>
2023-11-23 16:25:48 +0000

    undergraduate page files

---
+++ undergraduate.js
@@ -0,0 +1,605 @@
+function level100(){
+    document.querySelector('.js-courses').innerHTML=`FIRST SEMESTER COURSES`;
+}
+function firstsemester(){
+    document.querySelector('.js-firstsemester').innerHTML=`UGRC 150	Critical Thinking and Practical Reasoning
+  `;
+
+}
+function firstsemester1(){
+    document.querySelector('.js-firstsemester1').innerHTML=` MATH 121	Algebra and Trigonometry
+ `;
+
+}
+function firstsemester2(){
+    document.querySelector('.js-firstsemester2').innerHTML=` MATH 123	Vectors and Geometry
+ `;
+
+}
+function firstsemester3(){
+    document.querySelector('.js-firstsemester3').innerHTML=`   DCIT 101	Introduction to Computer Science
+ `;
+
+}
+function firstsemester4(){
+    document.querySelector('.js-firstsemester4').innerHTML=` DCIT 103	Office Productivity Tools
+ `;
+
+}
+function firstsemester5(){
+    document.querySelector('.js-firstsemester5').innerHTML=` STAT 111	Introduction to Statistics and Probability I
+`;
+
+}
+function level100_2(){
+    document.querySelector('.js-courses1').innerHTML=`SECOND SEMESTER COURSES`;
+}
+function secondsemester(){
+    document.querySelector('.js-secondsemester').innerHTML=`UGRC 110	Academic Writing I
+    
+    
+  `;
+
+}
+function secondsemester1(){
+    document.querySelector('.js-secondsemester1').innerHTML=` 
+    MATH 122	Calculus I
+    
+ `;
+
+}
+function secondsemester2(){
+    document.querySelector('.js-secondsemester2').innerHTML=` MATH 126	Algebra and Geometry
+   
+ `;
+
+}
+function secondsemester3(){
+    document.querySelector('.js-secondsemester3').innerHTML=`  STAT 112	Introduction to Statistics and Probability II
+`;
+}
+function secondsemester4(){
+    document.querySelector('.js-secondsemester4').innerHTML=` DCIT 104	Programming
+   
+ `;
+
+}
+function secondsemester5(){
+    document.querySelector('.js-secondsemester5').innerHTML=` DCIT 102	Computer Hardware Fund. and Circuits
+`;
+
+}
+function level200(){
+    document.querySelector('.level200_1').innerHTML=`FIRST SEMESTER COURSES`;
+}
+function level200_courses(){
+    document.querySelector('.level200_firstsemester').innerHTML=`${'UGRC 210	Academic Writing II '}\n 
+    `;
+}
+function level200_courses1(){
+    document.querySelector('.level200_firstsemester1').innerHTML=`
+    DCIT 201	Programming I
+ `;
+}
+function level200_courses2(){
+    document.querySelector('.level200_firstsemester2').innerHTML=` DCIT 203	Digital and Logic Systems Design
+ `;
+}
+function level200_courses3(){
+    document.querySelector('.level200_firstsemester3').innerHTML=`    DCIT 205	Multi Media and Web Design
+   `;
+}
+function level200_courses4(){
+    document.querySelector('.level200_firstsemester4').innerHTML=`   DCIT 207	Computer Architecture & Organisation
+    `;
+}
+function level200_courses5(){
+    document.querySelector('.level200_firstsemester5').innerHTML=`  MATH 223	Calculus II
+    `;
+}
+function level200_1(){
+    document.querySelector('.level200_2').innerHTML=`SECOND SEMESTER COURSES`;
+}
+function level200_courses01(){
+    document.querySelector('.level200_firstsemester01').innerHTML=`UGRC 220	Introduction to African Studies
+   
+    
+    `;
+}
+function level200_courses11(){
+    document.querySelector('.level200_firstsemester11').innerHTML=`
+    DCIT 202	Mobile Application Development
+   
+ `;
+}
+function level200_courses22(){
+    document.querySelector('.level200_firstsemester22').innerHTML=` DCIT 204	Data Structures & Algorithm I
+   
+ `;
+}
+function level200_courses33(){
+    document.querySelector('.level200_firstsemester33').innerHTML=`     DCIT206	Systems Administration
+  
+   `;
+}
+function level200_courses44(){
+    document.querySelector('.level200_firstsemester44').innerHTML=`     DCIT208	Software Engineering
+  
+    `;
+}
+function level200_courses55(){
+    document.querySelector('.level200_firstsemester55').innerHTML=`   DCIT 212	Numerical and Computational Methods
+    `;
+}
+
+
+
+
+function level300(){
+    document.querySelector('.level300_1').innerHTML=`FIRST SEMESTER COURSES`;
+}
+function level300_courses(){
+    document.querySelector('.level300_firstsemester').innerHTML=`DCIT 301	Operating Systems
+   
+    `;
+}
+function level300_courses1(){
+    document.querySelector('.level300_firstsemester1').innerHTML=`
+    DCIT 303	Computer Networks
+  
+ `;
+}
+function level300_courses2(){
+    document.querySelector('.level300_firstsemester2').innerHTML=`   DCIT 305	Database Fundamentals
+  
+    
+ `;
+}
+function level300_courses3(){
+    document.querySelector('.level300_firstsemester3').innerHTML=`      DCIT 313	Introduction to Artificial Intelligence
+  
+   `;
+}
+function level300_courses4(){
+    document.querySelector('.level300_firstsemester4').innerHTML=`     MATH 359	Discrete Mathematics
+    `;
+}
+function level300_courses5(){
+    document.querySelector('.level300_firstsemester5').innerHTML=`DCIT 307	Mini-Project
+   
+    `;
+}
+function level300_1(){
+    document.querySelector('.level300_2').innerHTML=`SECOND SEMESTER COURSES`;
+}
+function level300_courses01(){
+    document.querySelector('.level300_firstsemester01').innerHTML=`DCIT 400	Project
+   
+   
+    
+    `;
+}
+function level300_courses11(){
+    document.querySelector('.level300_firstsemester11').innerHTML=`
+    DCIT 401	Social, Legal, Ethical and Professional Issues
+   
+ `;
+}
+function level300_courses22(){
+    document.querySelector('.level300_firstsemester22').innerHTML=` DCIT 407	Image Processing
+    
+ `;
+}
+function level300_courses33(){
+    document.querySelector('.level300_firstsemester33').innerHTML=`     DCIT 306	Cloud Computing Numerical
+    and Computational Methods
+  
+   
+   `;
+}
+function level300_courses44(){
+    document.querySelector('.level300_firstsemester44').innerHTML=`      DCIT 316	Computational models for
+    Social Media Mining
+  
+    `;
+}
+function level300_courses55(){
+    document.querySelector('.level300_firstsemester55').innerHTML=`  DCIT 311	Machine Learning
+    `;
+}
+
+
+function level400(){
+    document.querySelector('.level400_1').innerHTML=`FIRST SEMESTER COURSES`;
+}
+function level400_courses(){
+    document.querySelector('.level400_firstsemester').innerHTML=`DCIT 400	Project
+   
+    `;
+}
+function level400_courses1(){
+    document.querySelector('.level400_firstsemester1').innerHTML=`
+    DCIT 401	Social, Legal, Ethical and Professional Issues
+   
+ `;
+}
+function level400_courses2(){
+    document.querySelector('.level400_firstsemester2').innerHTML=`  DCIT 407	Image Processing
+    ELECTIVES:
+   
+ `;
+}
+function level400_courses3(){
+    document.querySelector('.level400_firstsemester3').innerHTML=`     DCIT 403	Designing Intelligent Agents
+   
+   `;
+}
+function level400_courses4(){
+    document.querySelector('.level400_firstsemester4').innerHTML=`    DCIT 405	Statistical Models and Methods for Data Science
+   
+    `;
+}
+function level400_courses5(){
+    document.querySelector('.level400_firstsemester5').innerHTML=`  DCIT 411	Bioinformatics
+   
+    `;
+}
+function level400_1(){
+    document.querySelector('.level400_2').innerHTML=`SECOND SEMESTER COURSES`;
+}
+function level400_courses01(){
+    document.querySelector('.level400_firstsemester01').innerHTML=` DCIT 417	Network Performance Analysis and Modeling
+    
+   
+    
+    `;
+}
+function level400_courses11(){
+    document.querySelector('.level400_firstsemester11').innerHTML=`
+    DCIT 423	Network Servers and Infrastructure
+   
+ `;
+}
+function level400_courses22(){
+    document.querySelector('.level400_firstsemester22').innerHTML=`DCIT 402	Management Principles in Computing
+   
+   
+ `;
+}
+function level400_courses33(){
+    document.querySelector('.level400_firstsemester33').innerHTML=`     DCIT 418	Systems and Network Security
+    
+   `;
+}
+function level400_courses44(){
+    document.querySelector('.level400_firstsemester44').innerHTML=`     DCIT 428	Wireless Systems and Networks
+    ELECTIVES:
+   
+  
+    `;
+}
+function level400_courses55(){
+    document.querySelector('.level400_firstsemester55').innerHTML=`   DCIT 404	Advanced Databases
+    
+    `;
+}
+
+
+
+
+function level500(){
+    document.querySelector('.level500_1').innerHTML=`FIRST SEMESTER COURSES`;
+}
+function level500_courses(){
+    document.querySelector('.level500_firstsemester').innerHTML=`UGRC 150	Critical Thinking and Practical Reasoning
+    
+    `;
+}
+function level500_courses1(){
+    document.querySelector('.level500_firstsemester1').innerHTML=`
+    DCIT 101	Introduction to Computer Science
+   
+    
+ `;
+}
+function level500_courses2(){
+    document.querySelector('.level500_firstsemester2').innerHTML=` DCIT 103	Office Productivity Tools
+   
+ `;
+}
+function level500_courses3(){
+    document.querySelector('.level500_firstsemester3').innerHTML=`    DCIT 105	Mathematics for IT Professionals
+   
+   `;
+}
+function level500_courses4(){
+    document.querySelector('.level500_firstsemester4').innerHTML=`   STAT 111	Introduction to Statistics and Probability I
+    ELECTIVES:
+    
+    `;
+}
+function level500_courses5(){
+    document.querySelector('.level500_firstsemester5').innerHTML=` 
+    ECON 101	Introduction to Economics 
+    `;
+}
+function level500_1(){
+    document.querySelector('.level500_2').innerHTML=`SECOND SEMESTER COURSES`;
+}
+function level500_courses01(){
+    document.querySelector('.level500_firstsemester01').innerHTML=`UGRC 110	Academic Writing I
+    
+    
+   
+    
+    `;
+}
+function level500_courses11(){
+    document.querySelector('.level500_firstsemester11').innerHTML=`
+    MATH 122	Calculus I
+     `;
+}
+function level500_courses22(){
+    document.querySelector('.level500_firstsemester22').innerHTML=` MATH 126	Algebra and Geometry
+    
+   
+ `;
+}
+function level500_courses33(){
+    document.querySelector('.level500_firstsemester33').innerHTML=`   STAT 112	Introduction to Statistics and Probability II
+    
+  
+   `;
+}
+function level500_courses44(){
+    document.querySelector('.level500_firstsemester44').innerHTML=`     DCITDCIT 104	Programming
+  
+  
+    `;
+}
+function level500_courses55(){
+    document.querySelector('.level500_firstsemester55').innerHTML=`     DCIT 102	Computer Hardware Fund. and Circuits
+    `;
+}
+
+
+
+function level600(){
+    document.querySelector('.level600_1').innerHTML=`FIRST SEMESTER COURSES`;
+}
+function level600_courses(){
+    document.querySelector('.level600_firstsemester').innerHTML=`
+  
+    `;
+}
+function level600_courses1(){
+    document.querySelector('.level600_firstsemester1').innerHTML=`
+    DCIT 201	Programming I
+ `;
+}
+function level600_courses2(){
+    document.querySelector('.level600_firstsemester2').innerHTML=` DCIT 203	Digital and Logic Systems Design
+ `;
+}
+function level600_courses3(){
+    document.querySelector('.level600_firstsemester3').innerHTML=`    DCIT 205	Multi Media and Web Design
+   `;
+}
+function level600_courses4(){
+    document.querySelector('.level600_firstsemester4').innerHTML=`   DCIT 207	Computer Architecture & Organisation
+    `;
+}
+function level600_courses5(){
+    document.querySelector('.level600_firstsemester5').innerHTML=`  
+    DCIT 209	E-Business Architectures
+    `;
+}
+function level600_1(){
+    document.querySelector('.level600_2').innerHTML=`SECOND SEMESTER COURSES`;
+}
+function level600_courses01(){
+    document.querySelector('.level600_firstsemester01').innerHTML=`  UGRC 210	Academic Writing II
+    
+   
+    
+    `;
+}
+function level600_courses11(){
+    document.querySelector('.level600_firstsemester11').innerHTML=`
+    DCIT 202	Mobile Application Development
+   
+ `;
+}
+function level600_courses22(){
+    document.querySelector('.level600_firstsemester22').innerHTML=` DCIT 204	Data Structures & Algorithm I
+   
+ `;
+}
+function level600_courses33(){
+    document.querySelector('.level600_firstsemester33').innerHTML=`     DCIT206	Systems Administration
+  
+   `;
+}
+function level600_courses44(){
+    document.querySelector('.level600_firstsemester44').innerHTML=`     DCIT208	Software Engineering
+  
+    `;
+}
+function level600_courses55(){
+    document.querySelector('.level600_firstsemester55').innerHTML=`   DCIT 212	Numerical and Computational Methods
+    `;
+}
+
+
+
+
+
+function level700(){
+    document.querySelector('.level700_1').innerHTML=`FIRST SEMESTER COURSES`;
+}
+function level700_courses(){
+    document.querySelector('.level700_firstsemester').innerHTML=`DCIT 301	Operating Systems
+   
+    `;
+}
+function level700_courses1(){
+    document.querySelector('.level700_firstsemester1').innerHTML=`
+    DCIT 303	Computer Networks
+   
+ `;
+}
+function level700_courses2(){
+    document.querySelector('.level700_firstsemester2').innerHTML=`  DCIT 305	Database Fundamentals
+  
+ `;
+}
+function level700_courses3(){
+    document.querySelector('.level700_firstsemester3').innerHTML=`     DCIT 313	Introduction to Artificial Intelligence
+   
+   `;
+}
+function level700_courses4(){
+    document.querySelector('.level700_firstsemester4').innerHTML=`    DCIT 317	IT Project Management
+    ELECTIVES:
+    
+    `;
+}
+function level700_courses5(){
+    document.querySelector('.level700_firstsemester5').innerHTML=`  DCIT 307	Mini-Project
+   
+    `;
+}
+function level700_1(){
+    document.querySelector('.level700_2').innerHTML=`SECOND SEMESTER COURSES`;
+}
+function level700_courses01(){
+    document.querySelector('.level700_firstsemester01').innerHTML=` DCIT 315	Principles of 3D Environment
+   
+   
+    
+    `;
+}
+function level700_courses11(){
+    document.querySelector('.level700_firstsemester11').innerHTML=`
+    DCIT 321	Software Evolution
+    
+    I
+   
+ `;
+}
+function level700_courses22(){
+    document.querySelector('.level700_firstsemester22').innerHTML=` 
+    DCIT 302	Human Computer Interaction
+    
+   
+ `;
+}
+function level700_courses33(){
+    document.querySelector('.level700_firstsemester33').innerHTML=`     DCIT 304	Research Methods
+    
+   `;
+}
+function level700_courses44(){
+    document.querySelector('.level700_firstsemester44').innerHTML=`     DCIT 308	Data Structures and Algorithms II
+    
+  
+    `;
+}
+function level700_courses55(){
+    document.querySelector('.level700_firstsemester55').innerHTML=`  DCIT 312	Information Security Management
+    
+    `;
+}
+
+
+
+
+function level800(){
+    document.querySelector('.level800_1').innerHTML=`FIRST SEMESTER COURSES`;
+}
+function level800_courses(){
+    document.querySelector('.level800_firstsemester').innerHTML=`DCIT 400	Project
+    
+    `;
+}
+function level800_courses1(){
+    document.querySelector('.level800_firstsemester1').innerHTML=`
+    DCIT 401	Social, Legal, Ethical and Professional Issues
+    
+ `;
+}
+function level800_courses2(){
+    document.querySelector('.level800_firstsemester2').innerHTML=` DCIT 409	Digital Forensics
+   
+ `;
+}
+function level800_courses3(){
+    document.querySelector('.level800_firstsemester3').innerHTML=`   
+    DCIT 413	Play and Games
+    
+   `;
+}
+function level800_courses4(){
+    document.querySelector('.level800_firstsemester4').innerHTML=`   DCIT 415	Advanced Software Engineering
+   
+    `;
+}
+function level800_courses5(){
+    document.querySelector('.level800_firstsemester5').innerHTML=`   DCIT 419	Agile Methods
+   
+    `;
+}
+function level800_1(){
+    document.querySelector('.level800_2').innerHTML=`SECOND SEMESTER COURSES`;
+}
+function level800_courses01(){
+    document.querySelector('.level800_firstsemester01').innerHTML=` DCIT 421	Persuasive Systems Development
+    
+    
+   
+   
+    
+    `;
+}
+function level800_courses11(){
+    document.querySelector('.level800_firstsemester11').innerHTML=`
+    
+    DCIT 402	Management Principles in Computing
+   
+ `;
+}
+function level800_courses22(){
+    document.querySelector('.level800_firstsemester22').innerHTML=`  DCIT 418	Systems and Network Security
+   
+   
+   
+ `;
+}
+function level800_courses33(){
+    document.querySelector('.level800_firstsemester33').innerHTML=`     DCIT 428	Wireless Systems and Networks
+    
+  
+   `;
+}
+function level800_courses44(){
+    document.querySelector('.level800_firstsemester44').innerHTML=`   DCIT 406	Advanced Computer Netwo
+    
+    
+  
+    `;
+}
+function level800_courses55(){
+    document.querySelector('.level800_firstsemester55').innerHTML=`  DCIT 404	Advanced Databases
+   rks
+    `;
+}
+function organ(){
+    document.querySelector('.org_word').innerHTML=`The University of Ghana is one of the Africa's most preeminent public universities. Our impact on individuals, our region and the world is profound. whether we are launching young people into a boundless future or confronting the grand challenges of our time through dauntless research. UG educates more than 38,000students annually, with approximately 2.64% being computer science students. We turn ideas into impact and transform the world around us. For more about our impact, visit our news site, UG News.`;
+}
+function organ1(){
+    document.querySelector('.org_word1').innerHTML=`If you are inspired by a big challenge, you've come to the right place! The computer science community at the univrersity of Ghana encourages it's people to be vibrant and brave in taking on new challenges that push boundries beyond imagination.`;
+}function organ2(){
+    document.querySelector('.org_word2').innerHTML=`Experience a UG-CDCD classroom from wherever you are. Take advantage of real-time lectures and personal interaction with professors through a suite of online delivery options, customizing your degree to fit your schedule. Our online classroom maintains the same level of academic rigor, while providing students with more ways of learning.`;
+}function organ3(){
+    document.querySelector('.org_word3').innerHTML=`So what defines our students, faculty and community members? Above all, its our belief in the infinite possibilitie and our unshakable optimism. Its a connection to others near and far. A hunger for socio-economic transformation that pushes us to tackle challenges and pursue progress. Its the conviction that together we can create a world of good. Join us on the journey.`;
+}
